9f5e459f5901dfec3fb5a7ad8cabe1e2ea981c5d
Tuan Ha <hatuan@email.cz>
2024-10-06 12:19:02 +0200

    add a mockup homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import "./reset.css";
+import "./styles.css";
+import imageRestaurantLogo from "./assets/restaurant-logo.png";
+
 console.log("Webpack Config Working!");
+
+const imageContainerDiv = document.querySelector(".image-container");
+const imgRestaurantLogo = document.createElement("img");
+imgRestaurantLogo.src = imageRestaurantLogo;
+imageContainerDiv.appendChild(imgRestaurantLogo);
